6d4ccdd5860b6ede6d079577548b119966bd859c
Ezaje Ahamed <ezajeahamed@gmail.com>
2026-06-13 03:00:17 +0530

    add DELETE endpoint, fix Pydantic examples, and update dependencies

--- app.py
+++ app.py
@@ -160,3 +160,16 @@ def update_patient(
     save_data(data)
 
     return JSONResponse(status_code=200, content={"message": "Patient updated successfully"})
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+    #load data from the JSON file
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    del data[patient_id]
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message": "Patient deleted successfully"})
